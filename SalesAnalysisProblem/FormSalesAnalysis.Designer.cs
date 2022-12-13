bd55e91c5dca52a3e7a73019454561e8a08ee4d7
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-13 12:09:14 +0200

    Add project files.

---
+++ SalesAnalysisProblem/FormSalesAnalysis.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SalesAnalysisProblem
+{
+    partial class FormSalesAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnShowAnalysis = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnShowAnalysis
+            // 
+            this.BtnShowAnalysis.Location = new System.Drawing.Point(37, 47);
+            this.BtnShowAnalysis.Name = "BtnShowAnalysis";
+            this.BtnShowAnalysis.Size = new System.Drawing.Size(179, 56);
+            this.BtnShowAnalysis.TabIndex = 0;
+            this.BtnShowAnalysis.Text = "Show Analysis";
+            this.BtnShowAnalysis.UseVisualStyleBackColor = true;
+            this.BtnShowAnalysis.Click += new System.EventHandler(this.BtnShowAnalysis_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(249, 47);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(467, 350);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormSalesAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnShowAnalysis);
+            this.Name = "FormSalesAnalysis";
+            this.Text = "Sales Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnShowAnalysis;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ SalesAnalysisProblem/FormSalesAnalysis.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SalesAnalysisProblem
+{
+    public partial class FormSalesAnalysis : Form
+    {
+        public FormSalesAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShowAnalysis_Click(object sender, EventArgs e)
+        {
+            int[,] salesTable = new int[11, 4];
+            int[] totalSalesPerStyle = new int[11];
+            int[] totalSalesPerColor = new int[4];
+            int totalSales = 0;
+
+            string path = Application.StartupPath + @"\sales.txt";
+            salesTable = LoadDataFromFile(path);
+
+            CalculateTotalSalesPerStyle(salesTable, totalSalesPerStyle);
+            CalculateTotalSalesPerColor(salesTable, totalSalesPerColor);
+            CalculateTotalSales(salesTable, ref totalSales);
+
+            DisplayAnalysis(salesTable, totalSalesPerStyle, totalSalesPerColor, totalSales);
+
+        }
+
+        private int[,] LoadDataFromFile(string path)
+        {
+            int[,] salesTable = new int[11, 4];
+
+            string line = "";
+            bool finished = false;
+
+            int style;
+            int color;
+            int sales;
+
+            StreamReader streamReader = new StreamReader(path);
+
+            while (finished == false)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    style = Convert.ToInt32(line);
+                    color = Convert.ToInt32(streamReader.ReadLine());
+                    sales = Convert.ToInt32(streamReader.ReadLine());
+
+                    salesTable[style, color] += sales;
+                }
+            }
+            return salesTable;
+        }
+
+
+        private void CalculateTotalSalesPerStyle(int[,] salesTable, int[] totalSalesPerStyle)
+        {
+            for (int r = 1; r <= 10; r++)
+            {
+                for (int c = 1; c <= 3; c++)
+                {
+                    totalSalesPerStyle[r] += salesTable[r, c];
+                }
+            }
+        }
+
+        private void CalculateTotalSalesPerColor(int[,] salesTable, int[] totalSalesPerColor)
+        {
+            for (int c = 1; c <= 3; c++)
+            {
+                for (int r = 1; r <= 10; r++)
+                {
+                    totalSalesPerColor[c] += salesTable[r, c];
+                }
+            }
+        }
+
+        private void CalculateTotalSales(int[,] salesTable, ref int totalSales)
+        {
+            for (int r = 1; r <= 10; r++)
+            {
+                for (int c = 1; c <= 3; c++)
+                {
+                    totalSales += salesTable[r, c];
+                }
+            }
+        }
+
+        private void DisplayAnalysis(int[,] salesTable, int[] totalSalesPerStyle, int[] totalSalesPerColor, int totalSales)
+        {
+            int[] style = new int[11];
+
+            TxtResult.Text += "Style#      Color1      Color2      Color3      Total" + Environment.NewLine;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                TxtResult.Text += i + "\t";
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    TxtResult.Text += salesTable[i, j] + "\t";
+                }
+
+                TxtResult.Text += totalSalesPerStyle[i] + Environment.NewLine;
+            }
+
+            TxtResult.Text += "Totals";
+
+            for (int i = 1; i < totalSalesPerColor.Length; i++)
+            {
+                TxtResult.Text += "\t" + totalSalesPerColor[i];
+            }
+
+            TxtResult.Text += "\t" + totalSales;
+            
+        }
+    }
+}
